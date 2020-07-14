c4465ef4b64dd8f0e15d2740da0c71f12e3cd662
Aditya Saxena <18676199+AdiUser@users.noreply.github.com>
2020-07-15 00:43:07 +0530

    Update PaytmConstants.java

--- src/main/java/com/web/hackercode/constants/PaytmConstants.java
+++ src/main/java/com/web/hackercode/constants/PaytmConstants.java
@@ -1,8 +1,8 @@
 package com.web.hackercode.constants;
 
 public class PaytmConstants {
-  public final static String MID="gGBHdw85164354789345";
-  public final static String MERCHANT_KEY="KcG@!_zK2Ukqyla!";
+  public final static String MID="<MID>";
+  public final static String MERCHANT_KEY="<KEY>";
   public final static String INDUSTRY_TYPE_ID="Retail";
   public final static String CHANNEL_ID="WEB";
   public final static String WEBSITE="DEFAULT";
